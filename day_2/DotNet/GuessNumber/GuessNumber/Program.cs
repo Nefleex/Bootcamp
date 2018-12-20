ee3ddd902abfa2b6c1025df1592113dd0c4e658b
Mika Knuuttila <mika.knuuttila@hotmail.com>
2018-12-20 16:48:59 +0200

    added functionalities to GuessNumber game

--- day_2/DotNet/GuessNumber/GuessNumber/Program.cs
+++ day_2/DotNet/GuessNumber/GuessNumber/Program.cs
@@ -15,25 +15,36 @@ namespace GuessNumber
             string userInput;
             int inputValue;
 
+            
+
             Console.Write("Number between 1 and 20 has been generated.\nGuess the number!: ");
 
             userInput = Console.ReadLine();
             inputValue = Convert.ToInt32(userInput);
 
-            while ((inputValue != randomNum) & (inputValue < 21 & inputValue > 0))
+            //TO-DO: Handling non-numberic input
+
+            while ((inputValue != randomNum) )
             {
-                Console.WriteLine("Wrong Number :(\nTry again: ");
+                    if (inputValue < 21 || inputValue > 0)
+                Console.WriteLine("Wrong Number \nTry again: ");
                 userInput = Console.ReadLine();
                 inputValue = Convert.ToInt32(userInput);
-            }
-            if (inputValue > 20 || inputValue > 0)
+
+                    if (inputValue > 20 || inputValue < 0)
             {
-                Console.WriteLine("Number you entered is not within required parameter (less than 1 or greater than 20");
+                Console.WriteLine("Number you entered is not within required parameter (less than 1 or greater than 20\nTry Again");
+                        userInput = Console.ReadLine();
+                inputValue = Convert.ToInt32(userInput);
             }
+            }
+            
             if ( inputValue == randomNum)
             {
                 Console.WriteLine("Correct! The random number was: " + randomNum);
+                Console.ReadLine();
             }
+            
             Console.ReadLine();
             
 
