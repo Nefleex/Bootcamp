1881432ea0a29aff67f86b99eba343e30251ed95
Mika Knuuttila <mika.knuuttila@hotmail.com>
2019-01-13 15:31:41 +0200

    Added database POST and GET

--- xhr-test/public/app.js
+++ xhr-test/public/app.js
@@ -1,16 +1,48 @@
-let xhr = new XMLHttpRequest();
-let formData = new FormData();
-const btn = document.querySelector(".btn");
+const btnGetPost = document.querySelector(".btn-get-posts");
+const btnSubmit = document.querySelector(".btn-submit");
+const firstField = document.querySelector(".first-name");
+const lastField = document.querySelector(".last-name");
+
+const form = document.querySelector(".myForm");
 
 submit = () => {
   console.log("clicked");
-  xhr.open("POST", "./database.txt", true);
-  xhr.onreadystatechange = function() {
-    if (xhr.status == 200 && xhr.readyState == 4) {
-      console.log(formData);
-      console.log(xhr.status + xhr.readyState);
-    }
+  const data = {
+    firstName: firstField.value,
+    lastName: lastField.value
   };
+
+  console.log(data);
+
+  fetch("http://localhost:3000/", {
+    method: "POST",
+    body: JSON.stringify(data),
+    headers: new Headers({
+      "Content-Type": "application/json"
+    })
+  })
+    .then(parsed => console.log(parsed))
+    .catch(err => console.log(err));
+
+  // Empty the fields
+  firstField.value = "";
+  lastField.value = "";
 };
 
-btn.addEventListener("click", submit);
+getPosts = () => {
+  fetch("http://localhost:3000/")
+    .then(res => res.json())
+
+    .then(results =>
+      results.forEach(element => {
+        let list = document.createElement("li");
+        list.innerHTML = element.firstName;
+        document.body.appendChild(list);
+      })
+    )
+    .then(x => console.log(x))
+    .catch(err => console.log(err));
+};
+
+btnGetPost.addEventListener("click", getPosts);
+btnSubmit.addEventListener("click", submit);
--- xhr-test/server/index.js
+++ xhr-test/server/index.js
@@ -1,34 +1,66 @@
 var express = require("express");
 var app = express();
 var mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/node-blog");
 var bodyParser = require("body-parser");
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.engine("html", require("ejs").renderFile);
-app.set("view engine", "html");
-var postSchema = new mongoose.Schema({ body: String });
-var Post = mongoose.model("Post", postSchema);
-// Routes
-app.get("/", (req, res) => {
-  const posts = Post.find();
-  Post.find({}, (err, posts) => {
-    res.send(posts).status(200);
-  });
+
+mongoose.connect("mongodb://localhost:27017/node-blog");
+
+var db = mongoose.connection;
+
+db.on("error", function(err) {
+  console.log("mongodb connection error: %s", err);
+  process.exit();
 });
-app.post("/addpost", (req, res) => {
-  var postData = new Post(req.body);
-  postData
-    .save()
-    .then(result => {
-      res.redirect("/");
-    })
-    .catch(err => {
+db.once("open", function() {
+  console.log("Successfully connected to mongodb");
+  app.emit("dbopen");
+});
+
+var cors = require("cors");
+// app.use(
+//   bodyParser.urlencoded({
+//     extended: true
+//   })
+// );
+app.use(cors());
+app.use(bodyParser.json());
+// mongoose
+//   .connect("mongodb://localhost:27017/node-blog")
+//   .then(() => console.log("Connected.").catch(err => console.log(err)));
+const userSchema = new mongoose.Schema({
+  body: String,
+  firstName: String,
+  lastName: String
+});
+
+const User = mongoose.model("User", userSchema);
+
+// Routes
+app.get("/", async (req, res) => {
+  console.log("GET request made.");
+  const result = await User.find();
+  if (!result) {
+    return res.send("Something went wrong.");
+  }
+  res.send(result);
+});
+app.post("/", async (req, res) => {
+  console.log(req.body.firstName);
+  let user = new User({
+    firstName: req.body.firstName,
+    lastName: req.body.lastName
+  });
+  await user.save();
+  if (
+    function(error) {
       res.status(400).send("Unable to save data");
-    });
+      return;
+    }
+  );
+  res.send(user).status(200);
 });
 
 // Listen
 app.listen(3000, () => {
-  console.log("Server listing on 3000");
+  console.log("Server listening on 3000");
 });
