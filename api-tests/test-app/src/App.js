7253c76a4f05008113907277686ce342d3bab1d9
Mika Knuuttila <mika.knuuttila@hotmail.com>
2019-01-14 18:43:28 +0200

    Added toggle to exclude/include past broadcasts of the day without making additional API calls

--- api-tests/test-app/src/App.js
+++ api-tests/test-app/src/App.js
@@ -8,6 +8,7 @@ class App extends Component {
     super();
     this.state = {
       isLoaded: false,
+      showExpired: false,
       apiData: "",
       yle1data: "",
       yle2data: "",
@@ -18,14 +19,20 @@ class App extends Component {
   // Button to hide expired programs
   // Styling
   // Fix hard-coded time offset
+  toggleShows = () => {
+    this.setState({ showExpired: !this.state.showExpired });
+    document.querySelector(".expired-shows").textContent === "Show Expired"
+      ? (document.querySelector(".expired-shows").textContent = "Hide Expired")
+      : (document.querySelector(".expired-shows").textContent = "Show Expired");
+  };
 
   componentDidMount() {
     const date = moment(new Date());
     const now = date.format("YYYY-MM-DDTHH:mm:ss.[0200]");
     console.log("Date " + now);
 
-    const yle1Url = `https://external.api.yle.fi/v1/programs/schedules.json?service=yle-areena \
-    &mediaobject=video&starttime=${now}&limit=5&${this.state.privateApiKey}`;
+    const yle1Url = `https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv1 \
+    &starttime=${now}&${this.state.privateApiKey}`;
     // const yle2Url = `https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv2 \
     // &limit=20&${this.state.privateApiKey}`;
     // const areenaUrl = `https://external.api.yle.fi/v1/programs/schedules.json?service=yle-areena \
@@ -51,7 +58,7 @@ class App extends Component {
     fetch(proxyurl + yle1Url)
       .then(response => response.json())
       .then(contents => this.setState({ yle1data: contents, isLoaded: true }))
-      .then(() => console.log(this.state.yle1data))
+      // .then(() => console.log(this.state.yle1data))
       .catch(err => console.log(err));
 
     // fetch(proxyurl + urlForAllChannels)
@@ -69,27 +76,48 @@ class App extends Component {
     const now = date.format("YYYY-MM-DDTHH:mm:ss.[0200]");
     return (
       <div className="App">
-        {
-          <Channel
-            title={"YLE1"}
+        <button className="expired-shows" onClick={this.toggleShows}>
+          Show Expired
+        </button>
+        <input type="checkbox" name="show-expired" value="" />
+        <hr />
+        <div className="channels">
+          {/* <Channel
+            title={"YLE1 ALL"}
             url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv1 \
               &starttime=${now}&limit=5&`}
+          /> */}
+          <Channel
+            title={"YLE1 NOW"}
+            url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv1 \
+              &starttime=${now}&limit=5&`}
+            isToggled={this.state.showExpired}
           />
-        }
-        <Channel
-          title={"YLE2"}
-          url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv2&starttime=${now}&limit=5&`}
-        />
-        <Channel
-          title={"YLE AREENA"}
-          url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-areena \
+          <Channel
+            title={"YLE1 ALL"}
+            url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv1 \
+              &`}
+            isToggled={this.state.showExpired}
+          />
+
+          <Channel
+            title={"YLE2"}
+            url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv2&starttime=${now}&limit=5&`}
+            isToggled={this.state.showExpired}
+          />
+          <Channel
+            title={"YLE AREENA"}
+            url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-areena \
             &starttime=${now}&limit=5&`}
-        />
-        <Channel
-          title={"YLE TEEMA"}
-          url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-teema-fem \
+            isToggled={this.state.showExpired}
+          />
+          <Channel
+            title={"YLE TEEMA"}
+            url={`https://external.api.yle.fi/v1/programs/schedules.json?service=yle-teema-fem \
             &starttime=${now}&limit=5&`}
-        />
+            isToggled={this.state.showExpired}
+          />
+        </div>
       </div>
     );
   }
--- api-tests/test-app/src/components/Channel.js
+++ api-tests/test-app/src/components/Channel.js
@@ -1,38 +1,112 @@
 import React, { Component } from "react";
+import { resolve } from "q";
 
 export default class Channel extends Component {
   constructor(props) {
     super();
     this.state = {
       isLoaded: false,
-      data: "",
-      privateApiKey: process.env.REACT_APP_API_KEY
+      showExpired: false,
+      allShows: "",
+      privateApiKey: process.env.REACT_APP_API_KEY,
+      freshShows: [],
+      allData: null
     };
   }
+
+  componentWillReceiveProps(props) {
+    this.setState({ showExpired: props.isToggled });
+  }
+
   componentWillMount() {
     const url = `${this.props.url}${this.state.privateApiKey}`;
     const proxyurl = "https://cors-anywhere.herokuapp.com/";
 
+    // Excluding past broadcasts.
     fetch(proxyurl + url)
       .then(response => response.json())
-      .then(contents => this.setState({ data: contents, isLoaded: true }))
-      // .then(() => console.log(this.state.data))
+      .then(contents => this.setState({ allShows: contents, isLoaded: true }))
+      .then(() => {
+        this.state.allShows.data.map(item =>
+          Date.parse(item.endTime) > Date.parse(new Date())
+            ? this.setState({
+                freshShows: this.state.freshShows.concat(item)
+              })
+            : null
+        );
+      })
+      .then(x => console.log(x))
       .catch(err => console.log(err));
+
+    // fetch(proxyurl + "https://external.api.yle.fi/v1/programs/schedules.json?service=yle-tv1 \
+    // &" ).then(response => response.json).then(contents => this.setState({}))
   }
+
+  stateTester = () => {
+    let { data } = this.state.allShows;
+    data.map(item => {
+      console.log(item.content.title.fi);
+    });
+    console.log(data[0].content.title.fi);
+    console.log(data[0].endTime);
+  };
+
+  //   updateState = (){
+  // return new Promise((resolve, reject) => {
+
+  // })
+  //   }
+
+  // App() {
+  //   return (
+  //     <div className="App">
+  //       {Object.entries(this.state.allShows).map(item => {
+  //         item.map(el => {
+  //           return <div>{el.data}</div>;
+  //         });
+  //       })}
+  //     </div>
+  //   );
+  // }
+
   render() {
+    let content = {};
+    let promise = new Promise((res, rej) => {
+      resolve(content);
+    });
     return (
       <div>
+        <button className="button" onClick={this.stateTester}>
+          Click me
+        </button>
         <h1>{this.props.title}</h1>
-        {this.state.isLoaded ? (
-          this.state.data.data.map((item, index) => (
-            <div key={index}>
-              {item.content.title.fi || item.content.title.sv}
-              <hr />
-            </div>
-          ))
-        ) : (
-          <div>Awaiting API end point...</div>
-        )}
+        <div>
+          {!this.state.isLoaded ? <div>Loading</div> : <div>Loaded</div>}{" "}
+        </div>
+        <div>
+          {this.state.isLoaded && this.state.showExpired
+            ? this.state.allShows.data.map((item, index) => (
+                <div key={index}>
+                  {item.content.title.fi || item.content.title.sv}
+                  <hr />
+                </div>
+              ))
+            : null}
+        </div>
+        {
+          /////////////////////////
+        }
+
+        {this.state.isLoaded && !this.state.showExpired
+          ? this.state.freshShows.map((item, index) => (
+              <div key={index}>
+                {" "}
+                <div>{Date.parse(item.startTime)}</div>
+                {item.content.title.fi || item.content.title.sv}
+                <hr />
+              </div>
+            ))
+          : null}
       </div>
     );
   }
